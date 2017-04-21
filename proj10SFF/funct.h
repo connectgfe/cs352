e769d7a06d3321141759a8a3f5b56ad2a30150c1
G Esposito <gesposito@email.arizona.edu>
2017-04-21 00:15:36 -0700

    new

---
+++ proj10SFF/funct.h
@@ -0,0 +1,60 @@
+
+typedef struct Node{
+  int ind;
+  char *name;
+  struct Node *next;
+}node;
+
+typedef struct List{
+  struct Node *head;
+}list;
+
+typedef struct Graph{
+
+  struct List *ver;
+  struct Graph *nextv;
+}graph;
+
+typedef struct Vert{
+  char *vname;
+  int vind;
+  int targ;
+  int *targof;
+  struct Cmnd *cmd;
+  struct Vert *next;
+}vert;
+
+typedef struct Cmnd{
+  char *comnd;
+  struct Cmnd *nextc;
+}cmnd;
+
+
+int status;
+extern int numV;
+int flag;
+int mrkr;
+FILE *fp;
+char *testln;
+char *targ;
+int mkcmnd;
+char *prcmnd;
+int *vtx;
+struct stat *sb1;
+struct stat *sb2;
+
+
+
+void addlist(struct Graph*,char*,vert*);
+void addvert(vert*,char*,struct Graph*,int);
+void printG2(struct Graph*);
+void printVlist(vert*);
+void addedge2(vert*,struct Graph*,char*,char*);
+int getindex(vert*,char*);
+void isconn(vert*,struct Graph*,int*,int,int);
+void setcmnd(vert*,char*,char*,graph*);
+char *getcmnd(vert*,char*);
+void settarg(vert*,char*);
+void buildm(vert*,struct Graph*,int*,int,int);
+int istarg(vert*,char*);
+void myfree(vert*,graph*);
